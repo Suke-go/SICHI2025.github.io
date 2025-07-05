46aaac6a508b5495940a112973d17c47ff185dfe
KS00Max <kosuke05816@icloud.com>
2025-07-05 17:27:22 +0900

    for build test

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'process.env.GITHUB_PAGES'
-      ? "reactailwind"
-      : "./",
+  base: '/SICHI2025.github.io/',
 
 });
